22606f596906ea3db0da56439a0c89ea4905f85c
Yamato Sanekata <21jy0216@jec.ac.jp>
2022-10-06 13:38:38 +0900

    add npmInstallBat

---
+++ npmInsall.bat
@@ -0,0 +1,4 @@
+..\node-v18.10.0-win-x64\npm -g config set proxy http://proxy00.jec.ac.jp:8080
+..\node-v18.10.0-win-x64\npm -g config set https-proxy http://proxy00.jec.ac.jp:8080
+..\node-v18.10.0-win-x64\npm -g config set registry http://registry.npmjs.org/
+..\node-v18.10.0-win-x64\npm install .
