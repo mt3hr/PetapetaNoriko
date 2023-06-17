412853b625e95475426069587b78874ef71631e1
mt3hr <ymt3shr@gmail.com>
2023-06-18 02:53:06 +0900

    fix tag struct style

--- src/view/HTMLTagStructViewLi.vue
+++ src/view/HTMLTagStructViewLi.vue
@@ -36,13 +36,13 @@
                     </v-card-title>
                     <v-row>
                         <v-col cols="auto">行数</v-col>
-                        <v-col cols="auto"><input class="textbox" @keypress.enter="initialize_table" type="number" v-model="table_rows"
-                                default="1" /></v-col>
+                        <v-col cols="auto"><input class="textbox" @keypress.enter="initialize_table" type="number"
+                                v-model="table_rows" default="1" /></v-col>
                     </v-row>
                     <v-row>
                         <v-col cols="auto">列数</v-col>
-                        <v-col cols="auto"><input class="textbox" @keypress.enter="initialize_table" type="number" v-model="table_cols"
-                                default="1" /></v-col>
+                        <v-col cols="auto"><input class="textbox" @keypress.enter="initialize_table" type="number"
+                                v-model="table_cols" default="1" /></v-col>
                     </v-row>
                     <v-row>
                         <v-col cols="auto">
@@ -67,8 +67,8 @@
                     </v-card-title>
                     <v-row>
                         <v-col cols="auto">アイテム数</v-col>
-                        <v-col cols="auto"><input class="textbox" @keypress.enter="initialize_ul" type="number" v-model="ul_items"
-                                default="1" /></v-col>
+                        <v-col cols="auto"><input class="textbox" @keypress.enter="initialize_ul" type="number"
+                                v-model="ul_items" default="1" /></v-col>
                     </v-row>
                     <v-row>
                         <v-col cols="auto">
@@ -93,8 +93,8 @@
                     </v-card-title>
                     <v-row>
                         <v-col cols="auto">アイテム数</v-col>
-                        <v-col cols="auto"><input class="textbox" @keypress.enter="initialize_ol" type="number" v-model="ol_items"
-                                default="1" /></v-col>
+                        <v-col cols="auto"><input class="textbox" @keypress.enter="initialize_ol" type="number"
+                                v-model="ol_items" default="1" /></v-col>
                     </v-row>
                     <v-row>
                         <v-col cols="auto">
@@ -119,8 +119,8 @@
                     </v-card-title>
                     <v-row>
                         <v-col cols="auto">URL</v-col>
-                        <v-col cols="auto"><input class="textbox" @keypress.enter="initialize_img" type="url" v-model="img_src"
-                                default="1" /></v-col>
+                        <v-col cols="auto"><input class="textbox" @keypress.enter="initialize_img" type="url"
+                                v-model="img_src" default="1" /></v-col>
                     </v-row>
                     <v-row>
                         <v-col cols="auto">
@@ -719,7 +719,10 @@ li {
 }
 
 .tag_struct_li {
-    width: max-content;
+    list-style-position: inline;
+    width: 260px;
+    overflow: hidden;
+    text-wrap: nowrap;
 }
 
 .init_dialog {
